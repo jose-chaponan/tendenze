76c7f0e10047e7b5482d8d59292a25eee5dfa9e5
José Iván Chapoñán Casiano <ivan@navike21.com>
2019-04-07 06:19:29 -0500

    cambios pre finales

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,83 @@
-let _callServices = () =>{
+let _callServices = () => {
   fetch('../json/services.json')
   .then(response => response.json())
   .then(data => {
-
-    console.log(data);
-  })
+    let services = document.getElementsByClassName('services');
+    let arrayData = data.services;
+    arrayData.map((val, index) =>{
+      let tagWrapp = document.createElement('div');
+      let imgWrapp = document.createElement('img');
+      let caption = document.createElement('h3');
+      caption.innerHTML = val.title;
+      tagWrapp.appendChild(imgWrapp).src = val.img;
+      tagWrapp.appendChild(caption).classList.add('align_center');
+      services[0].appendChild(tagWrapp).classList.add('itemServices');
+    });
+  });
 }
 
+let _callClients = () => {
+  fetch('../json/clients.json')
+    .then(response => response.json())
+    .then(data => {
+      let clientes = document.getElementsByClassName('clientes');
+      let arrayData = data.clientes;
+      arrayData.map((val, index) => {
+        let imgWrapp = document.createElement('img');
+        imgWrapp.width = val.width;
+        imgWrapp.title = val.title;
+        imgWrapp.classList.add('cli-'+val.tipo);
+        clientes[0].appendChild(imgWrapp).src = val.img
+      });
+    });
+}
+
+let _callGallery = () => {
+  fetch('../json/gallery.json')
+    .then(response => response.json())
+    .then(data => {
+      let gallery = document.getElementsByClassName('gallery');
+      let arrayData = data.gallery;
+      arrayData.map((val, index) => {
+        let tagWrapp = document.createElement('div');
+        tagWrapp.classList.add('itemGallery');
+        let imgWrapp = document.createElement('img');
+        imgWrapp.title = val.title;
+        imgWrapp.src = val.img;
+        tagWrapp.appendChild(imgWrapp);
+        gallery[0].appendChild(tagWrapp)
+      });
+    });
+}
+
+/*Fixed header */
+let sticky = 0;
+
+let _fixedHeader = () => {
+  let header = document.getElementById("header");
+  sticky = parseInt(header.offsetTop) + 300;
+
+  if (window.pageYOffset > sticky) {
+    header.classList.add("fixedHeader");
+  } else {
+    header.classList.remove("fixedHeader");
+  }
+}
+
+let _parallaxSlider = () => {
+    let slider = document.getElementById("sliderPrincipal");
+    let scrolled = window.pageYOffset;
+    slider.style.backgroundPosition = 'center ' + - (scrolled * 0.15) + 'px';
+}
+
+/*Ready functions */
+_callGallery();
 _callServices();
-// window.onload = () => {
-//   _callServices();
-// }
+_callClients();
+
+window.onload = () => {
+  window.onscroll = () => {
+    _parallaxSlider();
+    _fixedHeader();
+  }
+}
