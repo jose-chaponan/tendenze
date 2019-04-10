d71a36b5e1ea7a7c53ff3ac0cf8a8bb1650cc404
José Iván Chapoñán Casiano <ivan@navike21.com>
2019-04-10 02:41:18 -0500

    subiendo cambios implementacion de modal con video

--- js/scripts.js
+++ js/scripts.js
@@ -105,9 +105,91 @@ let _openHideMenu = () => {
   }
 }
 
+/*Youtube */
+let player;
+
+let _onYouTubeIframeAPIReady = (idVideo) => {
+  player = new YT.Player('video-placeholder', {
+    width: '100%',
+    height: '100%',
+    videoId: idVideo,
+    playerVars: {
+      enablejsapi: 1,
+      color: 'black',
+      // playlist: 'taJ60kskkns, FG0fTKAqZ5g',
+      origin: 'http://127.0.0.1:5500'
+    },
+    events: {
+      // onReady: _initialize()
+    }
+  });
+}
+
+// _initialize = () => {
+
+//   // player.setPlaybackQuality('hd1080');
+
+//   // // Update the controls on load
+//   // updateTimerDisplay();
+//   // updateProgressBar();
+
+//   // // Clear any old interval.
+//   // clearInterval(time_update_interval);
+
+//   // // Start interval to update elapsed time display and
+//   // // the elapsed part of the progress bar every second.
+//   // time_update_interval = setInterval(function () {
+//   //   updateTimerDisplay();
+//   //   updateProgressBar();
+//   // }, 1000)
+
+// }
+/** */
+
+
+let _openVideo = () => {
+  let videos = document.querySelectorAll('.videoButon');
+  for (let i = 0; i < videos.length; i++) {
+    videos[i].onclick = () =>{
+      let idVideo = videos[i].dataset.video;
+      player.cueVideoById(idVideo);
+      player.setPlaybackQuality('hd1080');
+      player.unMute();
+      player.setVolume(50);
+
+      document.getElementsByClassName('overflowVideo')[0].classList.add('activeOverflowVideo');
+      document.getElementsByClassName('modalWrappVideo')[0].classList.add('activeModalWrappVideo');
+
+      setTimeout(() => {
+        player.playVideo();
+      }, 500);
+    }
+  }
+}
+
+let _closeVideo = () =>{
+  let closeModalTg = document.getElementById('closeModal');
+
+  closeModalTg.onclick = (e) => {
+    e.preventDefault();
+
+    player.pauseVideo();
+    player.mute();
+
+    document.getElementsByClassName('overflowVideo')[0].classList.remove('activeOverflowVideo');
+    document.getElementsByClassName('modalWrappVideo')[0].classList.remove('activeModalWrappVideo');
+  }
+
+}
+ 
 window.onload = () => {
+  _onYouTubeIframeAPIReady(0);
+  _openVideo();
   _clickAnchorLink();
   _openHideMenu();
+  _closeVideo();
+  // _onYouTubeIframeAPIReady();
+
   window.onscroll = () => {
     _parallaxSlider();
     _fixedHeader();
